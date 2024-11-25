7324f46ee98e7951d1caf239f948e96a5b49b39d
koala <tiyashasharma.2002@gmail.com>
2024-11-25 23:56:13 +0530

    commit

--- database/DbConnection.js
+++ database/DbConnection.js
@@ -4,7 +4,7 @@ import validator from "validator";
 export const DbConnection = () => {
     mongoose
         .connect(process.env.MONGO_URI, {
-            dbName: "JOB_SEEKING_PORTAL" ,
+            dbName: "JOB_SEEKING" ,
         })
         .then(() => {
             console.log("connected to database!");
